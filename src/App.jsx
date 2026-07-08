04b12f6a38ed16fd1ceb0e521b81c04cef7a68e3
Mariyum26 <mariyumkhan1111@gmail.com>
2026-07-08 21:21:47 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <h2>Hello World from react!</h2>
+      </section>
+    </>
+  )
+}
+
+export default App
