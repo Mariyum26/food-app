448d52b11d32e7df60fbf19023e14cb1738ba2d0
Mariyum26 <mariyumkhan1111@gmail.com>
2026-07-09 14:48:53 +0530

    added tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 
-import './App.css'
-
 function App() {
 
   return (
     <>
       <section id="center">
-        <h2>Hello World from react!</h2>
+        <h2 class="text-3xl font-bold underline">Hello World from react!</h2>
       </section>
     </>
   )
