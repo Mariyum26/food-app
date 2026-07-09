76bd854d0a3d489706b5752f70937105970797d8
Mariyum26 <mariyumkhan1111@gmail.com>
2026-07-09 15:44:39 +0530

    responsive Header created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-
+import Header from "./Header.jsx"
 function App() {
 
   return (
     <>
-      <section id="center">
-        <h2 class="text-3xl font-bold underline">Hello World from react!</h2>
+      <section>
+        <Header/>
       </section>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,17 @@
+const Header=()=>{
+    return (
+        <>
+        <section class="flex justify-between items-center shadow-lg">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3944WcH3EFiu9ezAR34l7gHdMqMqQOXi8OLmbRhFVwA&s=10" class="w-[50px] lg:w-[100px]"></img>
+            <div>
+                <ul class="flex text-md mr-10 md:mr-20 md:text-lg ">
+                    <li class="mr-10 md:mr-20">About</li>
+                    <li class="mr-10 md:mr-20">Contact Us</li>
+                    <li>Search</li>
+                </ul>
+            </div>
+        </section>
+        </>
+    )
+}
+export default Header;
